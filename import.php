933def472da1af503e0a6a4080812950ccf55567
Michael Jeffrey <michaeldjeffrey@gmail.com>
2013-02-28 10:52:55 -0700

    Changed array to name of post fields as found in wordpress.

--- import.php
+++ import.php
@@ -15,14 +15,14 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
 	while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 		if($row != 1){
         	$post = array(
-				'facility_id'    => mysql_real_escape_string($data[0]), //number
-				'capacity'       => mysql_real_escape_string($data[1]), //number
-				'license_status' => mysql_real_escape_string($data[2]), //string
-				'facility_name'  => mysql_real_escape_string($data[3]), //string
-				'street_address' => mysql_real_escape_string($data[4]), //string
-				'city'           => mysql_real_escape_string($data[5]), //string
-				'state'          => mysql_real_escape_string($data[6]), //string
-				'zip_code'       => mysql_real_escape_string($data[7]) //number
+				'wpcf[facility-number]'=> mysql_real_escape_string($data[0]), //number
+				'wpcf[capacity]'       => mysql_real_escape_string($data[1]), //number
+				'wpcf[license-status]' => mysql_real_escape_string($data[2]), //string
+				'wpcf[facility-name]'  => mysql_real_escape_string($data[3]), //string
+				'wpcf[street-address]' => mysql_real_escape_string($data[4]), //string
+				'wpcf[city]'           => mysql_real_escape_string($data[5]), //string
+				'wpcf[state]'          => mysql_real_escape_string($data[6]), //string
+				'wpcf[zipcode]'        => mysql_real_escape_string($data[7]) //number
 				);
         	// to print the array
 			// print "<pre>";
@@ -46,3 +46,4 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
 
 
 ?>
+
